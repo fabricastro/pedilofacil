bf25f3b31cfe870998f5c67d896d09c0f305e73e
fabricastro <fabricastrook@gmail.com>
2023-11-23 12:18:25 -0300

    first commit

---
+++ src/components/pure/Main.jsx
@@ -0,0 +1,10 @@
+export const Main = () => {
+  
+  const prueba = document.getElementById('prueba')
+
+  return (
+    <main className="h-[900px] bg-purple-100 px-10 py-5 dark:bg-gray-800">
+      <div className="dark:text-purple-100" id="prueba" ></div>
+    </main>
+  );
+};
