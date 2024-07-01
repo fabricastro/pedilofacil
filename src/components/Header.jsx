efbbf77ba4865be7e09870a2a996a552abaa0da5
fabricastro <fabricastrook@gmail.com>
2024-07-01 16:20:05 -0300

    agrego logo y boton de ingresar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
-  
+
   return (
     <>
-      <section className="flex h-16 w-full items-center justify-between px-10 bg-[#f5f5f5]">
-        <h1 className="text-2xl font-bold text-[#ED4523]">
+      <section className="flex h-24 w-full items-center justify-between px-10 bg-[#f5f5f5]">
+        <h1 className="text-2xl flex justify-center items-center font-bold text-[#ED4523] pl-5">
+          <img src="./../../public/favicon.svg" alt="icon" className="pr-2" />
           Pedilo
-        </h1>        
+        </h1>
+        <Link
+                to={"/Login"}
+                className="me-2 rounded-lg bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+              >
+                <h2 className="text-center">Ingresar</h2>
+              </Link>
       </section>
     </>
   );
