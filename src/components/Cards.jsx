b4e0e44dd38065c63979b91a21b005ac05682ab1
fabricastro <fabricastrook@gmail.com>
2024-07-03 12:53:36 -0300

    cambio de diseño de las cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,7 @@
 import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
 import { useContext, useEffect, useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
-import { FaMinus, FaPlus  } from "react-icons/fa6";
+import { FaMinus, FaPlus } from "react-icons/fa6";
 import { CartContext } from "../contexts/ShoppingCartContext";
 
 export function Cards() {
@@ -41,50 +41,54 @@ export function Cards() {
     <div className="grid grid-cols-1 gap-2 sm:grid-cols-4">
       {products.map((product, index) => (
         <Card
-          className="cursor-default"
+          className="cursor-default flex-row h-[130px] items-center"
           shadow="sm"
           key={index}
         >
-          <CardBody className="overflow-visible p-0">
-            {getQuantityById(product.id) > 0 && 
-            <>
-              <span className="absolute right-3 top-3 z-50 rounded-full bg-white px-2">
-                {getQuantityById(product.id)}
-              </span>
-            <button className="absolute z-50 left-3 top-3 border-1 border-black rounded-md" onClick={() => removeItem(product.id, product.price)}>
-            <FaMinus
-              className="text-xl bg-white rounded-md p-1" 
-              />
-            </button>
-            <button className="absolute z-50 left-9 top-3 border-1 border-black rounded-md"  
-            onClick={() => {
-              addToCart(product.id, product.precio);
-            }}>
-              <FaPlus  
-              className="text-xl bg-white rounded-md p-1 "
-              />
-            </button>
+          <CardBody className="overflow-visible p-10">
+            {getQuantityById(product.id) > 0 &&
+              <>
+                <span className="absolute right-3 top-3 z-50 rounded-full bg-white px-2">
+                  {getQuantityById(product.id)}
+                </span>
+                <button className="absolute z-50 left-3 top-3 border-1 border-black rounded-md" onClick={() => removeItem(product.id, product.price)}>
+                  <FaMinus
+                    className="text-xl bg-white rounded-md p-1"
+                  />
+                </button>
+                <button className="absolute z-50 left-9 top-3 border-1 border-black rounded-md"
+                  onClick={() => {
+                    addToCart(product.id, product.precio);
+                  }}>
+                  <FaPlus
+                    className="text-xl bg-white rounded-md p-1 "
+                  />
+                </button>
               </>
             }
             <Image
-              shadow="sm"
               radius="lg"
               width="100%"
               alt=""
-              className="h-[140px] w-full object-cover"
+              className="h-[100px] w-[100px] object-cover"
               src={product.imagen}
             />
           </CardBody>
           <CardFooter className="justify-between text-small">
-            <b>{product.nombre}</b>
-            <p className="text-default-500">${product.precio}</p>
-            <button>
-              <FaShoppingCart
-                onClick={() => {
-                  addToCart(product.id, product.precio, product.nombre);
-                }}
-              />
-            </button>
+            <div>
+
+              <b>{product.nombre}</b>
+              <div className="pt-5 flex gap-6">
+              <p className="text-default-500">${product.precio}</p>
+                <button>
+                  <FaShoppingCart
+                    onClick={() => {
+                      addToCart(product.id, product.precio, product.nombre);
+                    }}
+                  />
+                </button>
+              </div>
+            </div>
           </CardFooter>
         </Card>
       ))}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,11 @@ export const Header = () => {
   return (
     <>
       <section className="flex h-24 w-full items-center justify-between px-10 bg-[#f5f5f5]">
-        <h1 className="text-2xl flex justify-center items-center font-bold text-[#ED4523] pl-5">
-          <img src={icon} alt="icon" className="pr-2" />
-          Pedilo
-        </h1>
+       <div>
+          <img src={icon} alt="icon" className="pr-2" width={60} />
+       </div>
+          
+     
         <Link
                 to={"/Login"}
                 className="me-2 rounded-lg bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
